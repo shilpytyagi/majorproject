68ef7da15e9a04978a57cef4361861338c9677d0
shilpytyagi <tyagi.056shilpy@gmail.com>
2020-04-05 00:47:58 +0530

    shilpy

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MiddleComponent } from './middle/middle.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EventComponent } from './event/event.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MiddleComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    EventComponent,
+    PortfolioComponent,
+    ContactComponent,
+    OrderPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
